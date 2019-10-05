706919597ce3e51cbad67dc653f78c082e9a670a
talkabitx <talkabitx@163.com>
2019-10-05 14:12:44 +0800

    init redis

---
+++ stgy/data_stgy.js
@@ -0,0 +1,35 @@
+const log = require('../log');
+var redis = require('redis');
+client = redis.createClient(6379, '127.0.0.1', {});
+
+class DataStgy {
+    static base_stgy(msg) {
+        //console.log(msg.tick.asks);
+        var ask1 = 0;
+        for (let x of msg.tick.asks) { 
+            //console.log("ask:"+x);
+            ask1 = x;
+            break;
+        }
+        var bid1 = 0;
+        for (let x of msg.tick.bids) { 
+            //console.log("bid:"+x);
+            bid1 = x;
+            break;
+        }
+        //log.logger.info(msg.tick);
+        //console.log(msg.tick.ch);
+        client.set(msg.tick.ch, msg.tick.toString());
+        log.logger.info("###{\"ch\":\""+msg.tick.ch+
+                    "\",\"ask\":\""+ask1+
+                    "\",\"bid\":\""+bid1+
+                    "\",\"spread\":\""+(ask1[0]-bid1[0])+"\"}###");
+    }
+    static t100_stgy(msg) {
+        //console.log("t100 data")
+
+    }
+}
+
+
+module.exports = DataStgy
